0312071fa4ecbfee8c270c37288e0834140cb8dd
jediegoc <123850451+jediegoc@users.noreply.github.com>
2025-04-18 14:10:36 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,49 @@
+#%%
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import Pinecone as Pinecone_lang
+from pinecone import Pinecone, ServerlessSpec
+#%%
+st.set_page_config(
+    page_title="Home"    
+)
+
+st.write("# Q&A with news sources")
+
+st.sidebar.success("Select an Option above")
+
+st.markdown(
+    """
+    **This App provides the following functionalities:**
+    - Q&A with news articles for selected companies
+    - Q&A with news articles for a range of companies
+    - Q&A with news articles for all companies
+"""
+)
+
+#%% Load RAG
+@st.cache_resource
+def load_RAG():
+    #Load env. variables
+    os.environ['OPENAI_API_KEY']=st.secrets['OPENAI_API_KEY']
+    #'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'
+    os.environ['PINECONE_API_KEY']=st.secrets['PINECONE_API_KEY']
+    #'pcsk_2Zwydu_5FZX16wb3hD2fnVLh9AHwUrhGPizs9GLR2jf2Gf8YRxvjYpgvXM6u7LEvjRy7Y6'
+    #%%  
+
+    pc=Pinecone(
+        api_key=os.environ['PINECONE_API_KEY'],
+        environment='gpc-starter'
+    )
+    embeddings = OpenAIEmbeddings()
+    index_name = "refinitivnews-texts"
+    index = pc.Index(index_name)
+    docsearch = Pinecone_lang.from_existing_index(index_name, embeddings) 
+
+    return docsearch
+
+docsearch=load_RAG()
+st.session_state.docsearch=docsearch
